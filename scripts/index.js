7681672bafde7615a85afb542298edbeb4423437
Ayush <114506088+ayushkanyal@users.noreply.github.com>
2023-11-21 22:29:17 +0530

    Initialize index.js file

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+var today = new Date();
+var input_day = document.getElementById("input_day").value;
+var input_month = document.getElementById("input_month").value;
+var input_year = document.getElementById("input_year").value;
+var result_days = document.getElementById("result_days").value;
+var result_months = document.getElementById("result_months").value;
+var result_years = document.getElementById("result_years").value;
